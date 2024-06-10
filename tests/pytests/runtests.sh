5a5dfd9fc60779a8989e1bfa67bd3c67610c9dd9
DvirDukhan <dvir@redis.com>
2024-06-10 09:50:03 +0300

    restored build redisjson

--- tests/pytests/runtests.sh
+++ tests/pytests/runtests.sh
@@ -197,7 +197,7 @@ setup_clang_sanitizer() {
 			fi
 			if [[ ! -f $BINROOT/RedisJSON/rejson.so ]]; then
 				echo Building RedisJSON ...
-				# BINROOT=$BINROOT/RedisJSON $ROOT/sbin/build-redisjson
+				BINROOT=$BINROOT/RedisJSON $ROOT/sbin/build-redisjson
 				export MODULE_FILE=$(mktemp /tmp/rejson.XXXX)
 				$ROOT/sbin/build-redisjson
 				REJSON_MODULE=$(cat $MODULE_FILE)
